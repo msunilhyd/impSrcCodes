bda1b11a933c6c1bca845301c9d076741b8b2254
msunilhyd <msunilhyd@gmail.com>
2020-11-06 16:32:51 +0530

    6-Nov-20 : Fri: Stack :- Binary tree left view and HeapSort

--- javaFullStack/cardatabase/src/main/java/com/packt/cardatabase/domain/Car.java
+++ javaFullStack/cardatabase/src/main/java/com/packt/cardatabase/domain/Car.java
@@ -1,9 +1,12 @@
 package com.packt.cardatabase.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Car {
@@ -13,9 +16,19 @@ public class Car {
 	private String brand, model, color, registerNumber;
 	private int year, price;
 	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "owner")
+	private Owner owner;
+	
+	public Owner getOwner() {
+		return owner;
+	}
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
 	public Car() {}
 	public Car(String brand, String model, String color, String registerNumber,
-			int year, int price) {
+			int year, int price, Owner owner) {
 		super();
 		this.brand = brand;
 		this.model = model;
@@ -23,6 +36,7 @@ public class Car {
 		this.registerNumber = registerNumber;
 		this.year = year;
 		this.price = price;
+		this.owner = owner;
 	}
 	
 	public String getBrand() {
