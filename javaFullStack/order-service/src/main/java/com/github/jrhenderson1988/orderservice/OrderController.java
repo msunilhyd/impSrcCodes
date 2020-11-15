54cd183148b592c6ce1dd585a55cf9196f025874
msunilhyd <msunilhyd@gmail.com>
2020-11-15 18:28:04 +0530

    15-Nov-20 : Sun: Added multiple order services and load balancer

--- javaFullStack/order-service/src/main/java/com/github/jrhenderson1988/orderservice/OrderController.java
+++ javaFullStack/order-service/src/main/java/com/github/jrhenderson1988/orderservice/OrderController.java
@@ -2,9 +2,13 @@ package com.github.jrhenderson1988.orderservice;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,9 +20,25 @@ public class OrderController {
             new Order(4, 1, "Product D"),
             new Order(5, 2, "Product E"));
 
+    private final Environment environment;
+    
+    @Autowired
+    public OrderController(final Environment environment) {
+    	this.environment = environment;
+    }
+    
     @GetMapping
-    public List<Order> getAllOrders() {
-        return orders;
+    public ResponseWrapper<List<Order>> getAllOrders(@RequestParam(required = false) Integer customerId) {
+    	
+    	if (customerId != null) {
+    		return new ResponseWrapper<>(
+    				environment,
+    				orders.stream()
+    				.filter(order -> customerId.equals(order.getCustomerId()))
+    				.collect(Collectors.toList()));
+    	}
+    	
+    	return new ResponseWrapper<>(environment, orders);
     }
 
     @GetMapping("/{id}")
